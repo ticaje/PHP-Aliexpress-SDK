b5a0dd57a0e6f9b62ae92641d602bf4ab100bed8
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2020-06-03 11:50:40 +0200

    Add useful mediator

---
+++ src/Api/Traits/Mediator/ApiComplexMediator.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Api Class
+ * @category    Ticaje
+ * @author      Max Demian <ticaje@filetea.me>
+ */
+
+namespace Ticaje\AeSdk\Api\Traits\Mediator;
+
+use Ticaje\AeSdk\Domain\Interfaces\Request\ProductSendingInterface as DomainProductPostPolicy;
+
+/**
+ * Trait ApiComplexMediator
+ * @package Ticaje\AeSdk\Api\Traits\Mediator
+ */
+trait ApiComplexMediator
+{
+    /**
+     * @param $method
+     * @return string
+     */
+    private function getRequestWrapper($method)
+    {
+        /** @var DomainProductPostPolicy $requester */
+        $requester = $this->pool->get(self::REQUEST_SERVICE_MAPPER[$method]);
+        $result = $requester->getParamsWrapperKey();
+        $this->pool->free($requester); // Free worker so being available now
+        return $result;
+    }
+}
